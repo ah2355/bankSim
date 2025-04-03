6217ac732be023fdca2202930821ea43af8d509f
No_Name <105019557+ah2355@users.noreply.github.com>
2025-04-03 09:19:09 -0400

    Fixed some stuff in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,10 +2,12 @@ export default function Dashboard({user, onLogout} : {user : any; onLogout : () 
     return (
         <div className = "p-5">
             <div className = "card shadow p-3 mb-5 bg-body rounded">
-                <h2 className = "mb-3"> Welcome, {user.username}!</h2>
+                <h2 className = "mb-3 text-center"> Welcome, {user.username}!</h2>
             </div>
-            <div>
-                <h3 className = "mb-3">Account Balance</h3>
+            <div className = "container mt-5">
+                <div className="row">
+                    <h3 className = "mb-3">Account Info</h3>
+                </div>
             </div>
             <button className = "btn btn-danger" onClick = {onLogout}>Logout</button>
         </div>
